86a91fcdd594eac60fda12cf989e557af4847c7f
Noxirity <94649429+Noxirity@users.noreply.github.com>
2022-11-13 12:26:12 +0000

    init

---
+++ src/ServerScriptService/Isometria.lua
@@ -0,0 +1,6 @@
+--[[
+    ISOMETRIA | A building module
+
+    Isometria was developed by @Noxiirity, for Immersion Inc. Isometria is registered under the MIT License,
+    you can read more about the MIT license here: 
+--]]
